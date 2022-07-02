c642c355c050273b61650bf8052b7a7b3d805a55
Matheus Valença <82548455+Matheus351@users.noreply.github.com>
2022-07-02 13:23:53 -0300

    Main

---
+++ Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        Lista objetoLista = new Lista();
+        int option = 1;
+        int cont=0;
+        Scanner ler = new Scanner( System.in );
+     do{
+         System.out.println("<<MENU>>\nDigite um número para:\n 1.Adicionar um novo filme\n 2.Alterar uma nota\n 3.Para ver os filmes\n 4.Para mudar uma descrição\n 0.Para encerrar");
+         option = ler.nextInt();
+       switch(option){
+           
+         case 1:
+           ler.nextLine();
+          cont=cont+1;
+            System.out.println("Insira um título:");
+         
+           String titulo = ler.nextLine();
+             
+            System.out.println("Insira uma descrição:");
+         
+            String descr = ler.nextLine();
+       
+            System.out.println("Insira uma nota(número inteiro):");
+         
+             int n = ler.nextInt();
+      
+             System.out.println("Qual o genero?:");
+             ler.nextLine();  
+             String genero = ler.nextLine();
+             Filme filme = new Filme(titulo,n,descr,genero);
+             //objetoLista.inserirFilme();
+              objetoLista.setLista(filme);
+           System.out.println("Filme de "+genero+ " inserido");
+          break;
+           case 2:
+                  System.out.println("Insira o nome do filme que quer mudar a nota:");
+           
+          ler.nextLine();
+          String nomeDoFilme = ler.nextLine();
+
+         
+           objetoLista.procurarFilme(nomeDoFilme,"mudarNota");
+           
+           
+           break;
+           case 3:
+           
+                System.out.println("Escolha uma opção:\n 1.Ver todos\n 2.Filtrar os de Aventura\n 3.Filtrar os de Ação");
+                 ler.nextLine();
+                 int opcao = ler.nextInt();
+                 if(opcao==1){
+                    objetoLista.mostrarFilmes();    
+                 }
+                 else if(opcao==2){
+                   objetoLista.mostrarFilmesAventura();
+                 }else{
+                   objetoLista.mostrarFilmesAcao();
+                 }
+                 System.out.println("E voce inseriu "+cont+" novos filmes");
+           
+           break;
+           case 4:
+                     System.out.println("Insira o nome do filme que quer mudar a descrição:");
+                      ler.nextLine();
+                      nomeDoFilme = ler.nextLine();
+                  
+                      objetoLista.procurarFilme(nomeDoFilme,"mudarDescricao");
+           
+           break;
+               case 0:
+           
+                   System.out.println("Programa encerrado");
+           
+               break;
+               default:
+                System.out.println("Número inválido");
+          }
+     }while(option!=0);    
+        
+    }
+}
